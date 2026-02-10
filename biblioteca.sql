21a4ae437268bc090f193e08685e346d2a104503
Miriam Fernández <myriam1869@gmail.com>
2026-02-10 10:02:53 +0100

    formateo codigo sql

--- biblioteca.sql
+++ biblioteca.sql
@@ -1,38 +1,73 @@
 create DATABASE biblioteca;
 
-create table usuarios ( 
-    id_usuario int AUTO_INCREMENT primary key, 
-    nombre varchar(50) not null, 
-    correo varchar(100), edad int);
+create table
+    usuarios (
+        id_usuario int AUTO_INCREMENT primary key,
+        nombre varchar(50) not null,
+        correo varchar(100),
+        edad int
+    );
 
-
-insert into libros (titulo, autor, anio) VALUES 
-    ("Don Quijote", "Miguel de Cervantes", 1605), 
-    ("1984", "George Orwell", 1949), 
+insert into
+    libros (titulo, autor, anio)
+VALUES
+    ("Don Quijote", "Miguel de Cervantes", 1605),
+    ("1984", "George Orwell", 1949),
     ("El principito", "Antoine de Saint-Exupéry", 1943);
 
-
-insert into usuarios (nombre, edad) values 
-    ("Ana", 22), 
-    ("Luis", 25), 
-    ("Marta", 18), 
+insert into
+    usuarios (nombre, edad)
+values
+    ("Ana", 22),
+    ("Luis", 25),
+    ("Marta", 18),
     ("Carlos", 30);
 
+update libros
+set
+    disponible = false
+where
+    titulo = "El Principito";
 
-update libros set disponible = false where titulo = "El Principito";
+update usuarios
+set
+    correo = "ana@correo.com"
+WHERE
+    id_usuario = 1;
 
-update usuarios set correo = "ana@correo.com" WHERE id_usuario = 1;
+update usuarios
+set
+    edad = + 1
+where
+    edad > 25;
 
-update usuarios set edad =+ 1 where edad > 25; --No está bien, le pone 1 a la edad
+--No está bien, le pone 1 a la edad
+update usuarios
+set
+    edad = 30
+where
+    nombre = "Carlos";
 
-update usuarios set edad = 30 where nombre = "Carlos";
+update usuarios
+set
+    edad = edad + 1
+where
+    edad > 25;
 
-update usuarios set edad = edad +1 where edad > 25;
+delete from libros
+where
+    id_libro = 2;
 
-delete from libros where id_libro = 2;
+delete from usuarios
+where
+    edad < 18;
 
-delete from usuarios where edad < 18;
+select
+    *
+from
+    usuarios;
 
-select * from usuarios;
-
-select * from libros;
+select
+    *
+from
+    libros;
